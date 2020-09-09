008a157b5ff7d4c1f167db7907d32a229a6242c0
penggaosong <983379850@qq.com>
2020-09-09 17:33:00 +0800

    修改主启动项

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -15,6 +15,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
 
-        System.out.println("项目启动成功");
+        System.out.println(" ^   ^");
+        System.out.println("   _  ");
     }
 }
